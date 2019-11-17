e5e170a0b9fd891e02f88ef095b49c73ad19ebaa
yang1127 <1536520643@qq.com>
2019-11-17 22:09:16 +0800

    练习使用stack_queue

---
+++ stack_queue/stack_queue/stack_queue.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//stack、queue类模板
+//int main()
+//{
+//	stack<int> s; //可任意给类型<>
+//	s.push(1);
+//	s.push(2);
+//	s.push(3);
+//	s.push(4);
+//	s.push(5);
+//
+//	while (!s.empty())
+//	{
+//		cout << s.top() << endl; //stack:后进先出,取栈顶元素 5 4 3 2 1
+//		s.pop();
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	queue<int> q; //可任意给类型<>
+//	q.push(1);
+//	q.push(2);
+//	q.push(3);
+//	q.push(4);
+//	q.push(5);
+//
+//	while (!q.empty())
+//	{
+//		cout << q.front() << endl; //queue:先进先出，取对头元素 1 2 3 4 5 
+//		q.pop();
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
