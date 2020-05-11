f708807955573014a8e57c6d3e0f66f8deb60401
yang1127 <1536520643@qq.com>
2020-05-11 22:23:04 +0800

    练习

--- 模板/模板/test.cpp
+++ 模板/模板/test.cpp
@@ -52,7 +52,9 @@ using namespace std;
 //		return true;
 //
 //	return false;
-//}////类模板特化
+//}
+
+////类模板特化
 //template<class T1, class T2>
 //class Data
 //{
@@ -158,3 +160,111 @@ using namespace std;
 //	system("pause");
 //	return 0;
 //}
+
+////求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+//
+//class Solution {
+//public:
+//	int Sum_Solution(int n) {
+//		int sum = n;
+//		sum && (sum += Sum_Solution(n - 1));
+//		return sum;
+//	}
+//};
+
+//template<class T>
+//bool IsEqual(T& left, T& right)
+//{
+//	return left == right;
+//}
+//
+//void Test()
+//{
+//	char* p1 = "hello";
+//	char* p2 = "world";
+//	if (IsEqual(p1, p2))
+//		cout << p1 << endl;
+//	else
+//		cout << p2 << endl;
+//}
+//
+//int main()
+//{
+//	Test();
+//	system("pause");
+//	return 0;
+//}
+
+//STL
+// 代表内置类型
+struct __true_type {};
+
+// 代表自定义类型
+struct __false_type {};
+
+template <class type>
+struct __type_traits
+{
+	typedef __false_type is_POD_type;
+};
+
+// 对所有内置类型进行特化
+template<>
+struct __type_traits<char>
+{
+	typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<signed char>
+{
+	typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<unsigned char>
+{
+	typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<int>
+{
+	typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<float>
+{
+	typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<double>
+{
+	typedef __true_type is_POD_type;
+};
+
+// 注意：在重载内置类型时，所有的内置类型都必须重载出来，包括有符号和无符号，比如：对于int类型，必须特化三个，int-- signed int -- unsigned int
+
+// 在需要区分内置类型与自定义类型的位置，标准库通常都是通过__true_type与__false_type给出一对重载的函数，然后用一个通用函数对其进行封装
+
+// 注意：第三个参数可以不提供名字，该参数最主要的作用就是让两个_copy函数形成重载
+template<class T>
+void _copy(T* dst, T* src, size_t n, __true_type)
+{
+	memcpy(dst, src, n * sizeof(T));
+}
+
+template<class T>
+void _copy(T* dst, T* src, size_t n, __false_type)
+{
+	for (size_t i = 0; i < n; ++i)
+		dst[i] = src[i];
+}
+
+template<class T>
+void Copy(T* dst, T* src, size_t n)
+{
+	_copy(dst, src, n, __type_traits<T>::is_POD_type());
+}
