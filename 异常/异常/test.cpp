cef42c32a11c055c444bf93c8e3dcf346b6900df
yang1127 <1536520643@qq.com>
2020-08-13 15:12:10 +0800

    异常

---
+++ 异常/异常/test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//double Division(int a, int b)
+//{
+//	// 当b == 0时抛出异常  抛出异常可以是任意类型的对象
+//	if (b == 0)
+//		throw "Division by zero condition!";
+//	else
+//		return ((double)a / (double)b);
+//}
+//
+//void Func()
+//{
+//	int len, time;
+//	cin >> len >> time;
+//	cout << Division(len, time) << endl;
+//}
+//
+//int main()
+//{
+//	try
+//	{
+//		Func();
+//	}
+//	catch (const char* errmsg)
+//	{
+//		cout << errmsg << endl;
+//	}
+//	catch (...) // 捕获任意没有匹配的类型的异常，防止程序异常终止。但是这样捕获的不知道发生了什么错误
+//	{
+//		cout << "未知异常" << endl;
+//	}
+//
+//	return 0;
+//}
+
